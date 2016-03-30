a1f08988fbf937539e263f928e0a713361bf9522
mendelB <mendelblesofsky@gmail.com>
2016-03-30 16:10:55 -0400

    Done

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,21 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, favoriteFood, removeAllVowels;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		favoriteFood = $filter('favoriteFood');
+    removeAllVowels = $filter('removeAllVowels');
 	}));
 
+  it('favoriteFood filters for food argument', function(){
+    var items = [{name: 'mendel', favoriteFood: 'pizza'}, {name: 'Joe', favoriteFood: 'kale'}];
+    var returnVal = favoriteFood(items, 'pizza')
+    expect(returnVal).toEqual([items[0]]);
+  });
+
+  it('removeAllVowels removes all vowels from string', function(){
+    expect(removeAllVowels('I am so cool')).toEqual(' m s cl');
+  });
 	
 });
